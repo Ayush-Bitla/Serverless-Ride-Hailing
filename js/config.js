59909858aa90cc5917c026f92e5818800bec11ab
Ayush Bitla <161107424+Ayush-Bitla@users.noreply.github.com>
2025-04-22 22:46:23 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_SuouZuGAv', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1idjcal85o43nhq114r91du8hs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_yvkTIFszb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5j6e22ptsbg8dfqibsaahg7042', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
